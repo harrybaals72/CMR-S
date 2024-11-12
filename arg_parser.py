faae37bbe93869d04579638000d34c991c4df3ee
nkosa427 <nkosa427@gmail.com>
2024-11-12 10:07:40 -0800

    1st test of file walk

--- arg_parser.py
+++ arg_parser.py
@@ -7,9 +7,9 @@ def parse_arguments():
     parser.add_argument('-o', '--output', type=str, default='/output', help='Output folder for the database')
     parser.add_argument('-s', '--soft-run', action='store_true', help='Run the scraper without writing to the database')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output (set log level to DEBUG)')
-    parser.add_argument('--log-level', type=str, default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                        help='Set the logging level')
+    parser.add_argument('--log-level', type=str, default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Set the logging level')
     parser.add_argument('--no-scrape', action='store_true', help='Disable scraping (default: False)')
     parser.add_argument('-u', '--update', action='store_true', help='Update the database with local files')
+    parser.add_argument('--file-path', type=str, default='/userData/', help='Local directory with files to match. Default is /userData/')
 
     return parser.parse_args()
