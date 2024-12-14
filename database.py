93b08bd694259ffb4f7af543d4fe8ba485a4081a
nkosa427 <nkosa427@gmail.com>
2024-12-14 10:29:57 -0800

    Just gonna reset all the databases

--- database.py
+++ database.py
@@ -4,12 +4,11 @@ import logging
 
 logger = logging.getLogger(__name__) 
 
-def create_or_update_db(data, db_path, overwrite):
+def create_db(db_path, conn, overwrite):
 	if overwrite and os.path.exists(db_path):
 		logger.info(f"Deleting existing database at {db_path}")
 		os.remove(db_path)
 
-	conn = sqlite3.connect(db_path)
 	cursor = conn.cursor()
 
 	# Check if the 'posts' table exists
@@ -36,28 +35,75 @@ def create_or_update_db(data, db_path, overwrite):
 				downloaded INTEGER DEFAULT 0
 			)
 		''')
+		conn.commit()
 	
-	# Insert new entries
-	for post_id, date, text, serverFileName, serverPath, post_url, mediaType in data:
-		logger.debug(f"Checking if filename {serverFileName} already exists in the database")
-		cursor.execute('''
-			SELECT COUNT(*) FROM posts
-			WHERE post_id = ? AND serverFileName = ?
-		''', (post_id, serverFileName))
-		row_count = cursor.fetchone()[0]
+	cursor.close()  # Explicitly close the cursor
+	################################### END OF CREATE_DB ###################################
 
-		if row_count == 0:
-			logger.debug(f"Inserting post ID {post_id} into the database")
-			cursor.execute('''
-				INSERT INTO posts (post_id, date, text, serverFileName, serverPath, post_url, mediaType)
-				VALUES (?, ?, ?, ?, ?, ?, ?)
-			''', (post_id, date, text, serverFileName, serverPath, post_url, mediaType))
-		else:
-			logger.debug(f"Post ID {post_id} with serverFileName {serverFileName} already exists in the database, skipping insert")
+
+def insert_new_row(conn, post_id, date, text, serverFileName, serverPath, post_url, mediaType):
+	cursor = conn.cursor()
+
+	logger.debug(f"Inserting new row with post ID {post_id}, serverFileName {serverFileName} into the database")
+	cursor.execute('''
+		INSERT INTO posts (post_id, date, text, serverFileName, serverPath, post_url, mediaType)
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+	''', (post_id, date, text, serverFileName, serverPath, post_url, mediaType))
 
 	conn.commit()
 	cursor.close()  # Explicitly close the cursor
+	################################### END OF INSERT_NEW_ROW ###################################
+
+def update_row(conn, post_id, date, text, serverFileName, serverPath, post_url, mediaType):
+	cursor = conn.cursor()
+
+	logger.debug(f"Updating row with post ID {post_id}, serverFileName {serverFileName} in the database")
+	cursor.execute('''
+		UPDATE posts
+		SET date = ?, text = ?, serverFileName = ?, serverPath = ?, post_url = ?, mediaType = ?
+		WHERE post_id = ?
+	''', (date, text, serverFileName, serverPath, post_url, mediaType, post_id))
+
+	conn.commit()
+	cursor.close()  # Explicitly close the cursor
+	################################### END OF UPDATE_ROW ###################################
+
+
+def update_db(conn, data):
+	cursor = conn.cursor()
+
+	for post_id, date, text, serverFileName, serverPath, post_url, mediaType in data:
+		# Check if a row with a matching post_id and serverFileName is already exists in the database
+		cursor.execute('''
+			SELECT * FROM posts
+			WHERE post_id = ? AND serverFileName = ?
+		''', (post_id, serverFileName))
+		rows = cursor.fetchall()
+
+		if len(rows) == 0:
+			logger.debug(f"No rows found with post ID {post_id} and serverFileName {serverFileName}, inserting new row")
+			insert_new_row(conn, post_id, date, text, serverFileName, serverPath, post_url, mediaType)
+		elif len(rows) == 1:
+			logger.debug(f"Row with post ID {post_id} and serverFileName {serverFileName} already exists, updating row")
+			update_row(conn, post_id, date, text, serverFileName, serverPath, post_url, mediaType)
+		else:
+			logger.error(f"Multiple rows found with post ID {post_id} and serverFileName {serverFileName}, skipping update")
+			raise Exception(f"Multiple rows found with post ID {post_id} and serverFileName {serverFileName}, which is unexpected.")
+		
+	conn.commit()
+	cursor.close()  # Explicitly close the cursor
+	################################### END OF UPDATE_DB ###################################
+
+
+def create_or_update_db(data, db_path, overwrite):
+	conn = sqlite3.connect(db_path)
+	conn.row_factory = sqlite3.Row
+
+	create_db(db_path, conn, overwrite)
+	update_db(conn, data)
 	conn.close()
+	################################### END OF CREATE_OR_UPDATE_DB ###################################
+
 
 def get_undownloaded_video_posts_from_db(db_path):
 	logger.debug(f"Fetching undownloaded video posts from database: {db_path}")
@@ -75,3 +121,4 @@ def get_undownloaded_video_posts_from_db(db_path):
 	conn.close()
 
 	return rows
+	################################### END OF GET_UNDOWNLOADED_VIDEO_POSTS_FROM_DB ###################################
